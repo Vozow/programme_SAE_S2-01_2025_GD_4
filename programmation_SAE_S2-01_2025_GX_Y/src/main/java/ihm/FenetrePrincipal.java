5276689009dd42e6d0b8aa907d44a4bb17f7e827
ilyas <ilyas@PC-ILYAS>
2025-05-27 12:23:55 +0200

    Dev FenetrePrincipal

--- programmation_SAE_S2-01_2025_GX_Y/src/main/java/ihm/FenetrePrincipal.java
+++ programmation_SAE_S2-01_2025_GX_Y/src/main/java/ihm/FenetrePrincipal.java
@@ -1,7 +1,6 @@
 package ihm;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,13 +8,21 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import modèle.*;
 
 public class FenetrePrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Tomates tomates;
+	
 	private JPanel contentPane;
+	private DefaultListModel<String> modeleList;
 
 	/**
 	 * Launch the application.
@@ -24,7 +31,10 @@ public class FenetrePrincipal extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					FenetrePrincipal frame = new FenetrePrincipal();
+					//Initialisation de la 
+					Tomates tomates = OutilsBaseDonneesTomates.générationBaseDeTomates("./src/main/resources/data/tomates.json");
+					//Lancement de la fenetre
+					FenetrePrincipal frame = new FenetrePrincipal(tomates);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +49,10 @@ public class FenetrePrincipal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FenetrePrincipal() {
+	public FenetrePrincipal(Tomates tomates) {
+		
+		this.tomates = tomates;
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -49,20 +62,58 @@ public class FenetrePrincipal extends JFrame {
 		//Border Layout principal
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		//Création du panel de tête
 		JPanel headpanel = new JPanel();
 		contentPane.add(headpanel, BorderLayout.NORTH);
 		headpanel.setLayout(new BorderLayout(0, 0));
 		
+		//Création du titre
 		JLabel lblTitle = new JLabel("O'Tomates");
-		lblTitle.setFont(new Font("Century Gothic", Font.BOLD, 30));
+		lblTitle.setIcon(new ImageIcon("C:\\Users\\ilyas\\git\\repository\\programmation_SAE_S2-01_2025_GX_Y\\src\\main\\resources\\images\\Icones\\tomates_resize1.png"));
+		lblTitle.setFont(new Font("Ebrima", Font.BOLD, 30));
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
 		headpanel.add(lblTitle, BorderLayout.CENTER);
 		
-		JButton btnPanier = new JButton("0.00 €");
-		btnPanier.setIcon(null);
+		//Création du bouton Panier
+		JButton btnPanier = new JButton("-.-- €");
+		btnPanier.setFont(new Font("Ebrima", Font.BOLD, 15));
+		btnPanier.setIcon(new ImageIcon("C:\\Users\\ilyas\\git\\repository\\programmation_SAE_S2-01_2025_GX_Y\\src\\main\\resources\\images\\Icones\\buy_resize2.png"));
 		headpanel.add(btnPanier, BorderLayout.EAST);
 		
-		//
+
+		
+		JScrollPane ListScrollPane = new JScrollPane();
+		contentPane.add(ListScrollPane, BorderLayout.CENTER);
+		
+
+		//Création du modèle de la liste listTomates
+		this.modeleList = new DefaultListModel<String>();
+		//Création de la listTomates
+		JList<String> listTomates = new JList<String>(this.modeleList);
+		listTomates.setFont(new Font("Ebrima", Font.PLAIN, 15));
+		ListScrollPane.setViewportView(listTomates);
+		listTomates.setModel(this.modeleList);
+
+		
+		JPanel bottomPanel = new JPanel();
+		contentPane.add(bottomPanel, BorderLayout.SOUTH);
+		
+		
+		//Actualisation du bouton panier
+		
+		
+		//Actualisation de la table tableTomates
+		this.actualiserListTomates();
+	}
+
+
+
+
+	private void actualiserListTomates() {
+		this.modeleList.setSize(0);
+		for(Tomate tomate : this.tomates.getTomates()) {
+			this.modeleList.add(this.modeleList.size(), tomate.getDésignation());
+		}
 	}
 
 }
