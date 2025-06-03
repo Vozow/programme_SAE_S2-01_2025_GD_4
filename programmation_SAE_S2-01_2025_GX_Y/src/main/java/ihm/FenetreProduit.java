b5e642cc400fe061acefd0ec22e8dba7cc1b0252
ilyas <ilyas@PC-ILYAS>
2025-06-03 18:21:34 +0200

    Image + page produit

--- programmation_SAE_S2-01_2025_GX_Y/src/main/java/ihm/FenetreProduit.java
+++ programmation_SAE_S2-01_2025_GX_Y/src/main/java/ihm/FenetreProduit.java
@@ -1,44 +1,47 @@
 package ihm;
-
 import modèle.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.JSpinner;
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import java.awt.SystemColor;
+import java.text.DecimalFormat;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.BoxLayout;
+import java.awt.Component;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 public class FenetreProduit extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	
-	private Tomate tomate;
-	
+		
 	private JPanel contentPane;
-	private JTextField textFieldNbGraine;
-
+	
+	private JSpinner spinnerQteAchat;
+	private JLabel lblPrixTotal;
+	private Tomate tomate;
+	private DecimalFormat df = new DecimalFormat("0.00");
 
 	public FenetreProduit(Tomate tomate) {
-		this.tomate = tomate;
 		
+		this.tomate = tomate; 
+	
 		//Parametre princiapl
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 600, 500);
+		setBounds(100, 100, 600, 550);
 		setModal(true);;
 		setTitle("O'Tomates - " + tomate.getDésignation());
 		setIconImage(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\tomates_resize1.png").getImage());
@@ -50,31 +53,37 @@ public class FenetreProduit extends JDialog {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		
-		//Création du panel de titre
-		JPanel titlePanel = new JPanel();
-		contentPane.add(titlePanel, BorderLayout.NORTH);
-		titlePanel.setLayout(new BorderLayout(0, 0));
-		
 		//Création du titre de la tomate
 		JLabel lblTitlePanel = new JLabel(tomate.getDésignation());
 		lblTitlePanel.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\tomates_resize1.png"));
 		lblTitlePanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		lblTitlePanel.setFont(new Font("Ebrima", Font.BOLD, 25));
 		lblTitlePanel.setHorizontalAlignment(SwingConstants.CENTER);
-		titlePanel.add(lblTitlePanel, BorderLayout.CENTER);
+		contentPane.add(lblTitlePanel, BorderLayout.NORTH);
 		
 		
-		//Création du panel d'information principal
+		//Création du panel principal
+		JPanel mainPanel = new JPanel();
+		contentPane.add(mainPanel, BorderLayout.CENTER);
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		mainPanel.setBorder(new LineBorder(Color.lightGray, 2));
+		mainPanel.setBackground(Color.white);
+		
+		//Création du panel contenant l'image, le stock et les suggestion 
 		JPanel infoPanel = new JPanel();
-		contentPane.add(infoPanel, BorderLayout.CENTER);
-		infoPanel.setLayout(new BorderLayout(0, 0));
+		mainPanel.add(infoPanel);
+		infoPanel.setOpaque(false);
+		infoPanel.setBorder(new LineBorder(new Color(100, 100, 120), 1));
+		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
 		
 		//Création de l'image de la tomate
 		JLabel lblImageTomate = new JLabel();
+		lblImageTomate.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblImageTomate.setVerticalAlignment(SwingConstants.TOP);
 		lblImageTomate.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Tomates200x200\\" + tomate.getNomImage() + ".jpg"));
 		lblImageTomate.setHorizontalAlignment(SwingConstants.CENTER);
-		lblImageTomate.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-		infoPanel.add(lblImageTomate, BorderLayout.WEST);
+		lblImageTomate.setBorder(new EmptyBorder(10, 10, 10, 10));
+		infoPanel.add(lblImageTomate);
 		
 		//Création de la description
 		JTextArea TextAreaDescription = new JTextArea(tomate.getDescription());
@@ -84,94 +93,124 @@ public class FenetreProduit extends JDialog {
 		TextAreaDescription.setLineWrap(true);
 		TextAreaDescription.setWrapStyleWord(true);
 		TextAreaDescription.setOpaque(false);
-		TextAreaDescription.setBorder(new EmptyBorder(10, 20, 0, 0));
-		infoPanel.add(TextAreaDescription, BorderLayout.CENTER);
+		infoPanel.add(TextAreaDescription);
 		
-		//Création du panel d'achat
-		JPanel buyPanel = new JPanel();
-		contentPane.add(buyPanel, BorderLayout.SOUTH);
-		buyPanel.setBorder(new EmptyBorder(10, 0, 10, 0));
-		buyPanel.setLayout(new BorderLayout(0, 0));
+		
 		
 		//Création du label stock
-		JLabel lblStockTomate = new JLabel("En Stock : " + String.valueOf(tomate.getStock()));
-		lblStockTomate.setFont(new Font("Tahoma", Font.BOLD, 20));
+		JLabel lblStockTomate = new JLabel();
+		lblStockTomate.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblStockTomate.setFont(new Font("Ebrima", Font.BOLD, 20));
 		lblStockTomate.setHorizontalAlignment(SwingConstants.CENTER);
-		lblStockTomate.setBorder(new LineBorder(Color.gray, 1));
-		buyPanel.add(lblStockTomate, BorderLayout.NORTH);
-
-		
-		//Création du panel gauche contenant le stock et les suggestion
-		JPanel leftPanel = new JPanel();
-		buyPanel.add(leftPanel, BorderLayout.WEST);
-		leftPanel.setLayout(new GridLayout(2, 1, 0, 0));
+		lblStockTomate.setBorder(new EmptyBorder(20, 10, 10, 10));
+		if(tomate.getStock() > 0) {
+			lblStockTomate.setText("En Stock : " + String.valueOf(tomate.getStock()));
+			lblStockTomate.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\stock_resize2.png"));
+		}
+		else {
+			lblStockTomate.setText("Rupture de stock");
+			lblStockTomate.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\out_of_stock_resize2.png"));
+		}
+		mainPanel.add(lblStockTomate);
 		
 		//Création de la comboBox suggerant les tomates
-		JComboBox<Tomate> comboBoxTomateSuggerer = new JComboBox<Tomate>();
-		comboBoxTomateSuggerer.setEnabled(false);
-		leftPanel.add(comboBoxTomateSuggerer, BorderLayout.SOUTH);
-		
-		
-		//Création du panel droit contenant la description, le nombre de graine, le prix et l'achat
-		JPanel rightPanel = new JPanel();
-		buyPanel.add(rightPanel, BorderLayout.EAST);
-		rightPanel.setLayout(new GridLayout(3, 1, 0, 0));
-		
-
-		
-		//Création du label contenant le nombre de graine
-		JLabel lblGraine = new JLabel("Nombre de graine : " + String.valueOf(tomate.getNbGrainesParSachet()));
-		lblGraine.setHorizontalAlignment(SwingConstants.CENTER);
-		lblGraine.setFont(new Font("Ebrima", Font.BOLD, 15));
-		lblGraine.setBorder(new EmptyBorder(10, 10, 10, 10));
-		rightPanel.add(lblGraine);
-		
-		//Création du label prix
-		JLabel lblPrix = new JLabel("Prix Unitaire : " + tomate.getPrixTTC());
-		lblPrix.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPrix.setFont(new Font("Ebrima", Font.BOLD, 15));
-		lblPrix.setBorder(new EmptyBorder(10, 10, 10, 10));
-		rightPanel.add(lblPrix);
-		
-		
-		//Création du panelPrix contenant le prix et le choix d'achat
+		if(tomate.getStock() == 0) {
+			JComboBox<Tomate> comboBoxTomateSuggerer = new JComboBox<Tomate>();
+			comboBoxTomateSuggerer.setEnabled(false);
+			mainPanel.add(comboBoxTomateSuggerer);
+		}
+			
+		//Création du panelPrix contenant le prix total et le choix d'achat
 		JPanel prixPanel = new JPanel();
-		rightPanel.add(prixPanel);
-		prixPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-		prixPanel.setLayout(new GridLayout(1, 3, 0, 0));
+		mainPanel.add(prixPanel);
+		prixPanel.setBorder(new EmptyBorder(10, 10, 0, 10));
+		prixPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		prixPanel.setOpaque(false);
 		
 		//Création le label prix total
 		JLabel lblPrixTotal = new JLabel("Prix Total : ");
-		lblPrixTotal.setBorder(new EmptyBorder(10, 10, 10, 10));
+		lblPrixTotal.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\buy_resize2.png"));
 		lblPrixTotal.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPrixTotal.setFont(new Font("Ebrima", Font.PLAIN, 14));
+		lblPrixTotal.setFont(new Font("Ebrima", Font.PLAIN, 15));
 		prixPanel.add(lblPrixTotal);
 		
 		//Création du champ de texte contenant le prix totale
-		JTextArea TextAreaPrixTotal = new JTextArea("-.--€");
-		TextAreaPrixTotal.setBorder(new EmptyBorder(10, 10, 10, 10));
-		TextAreaPrixTotal.setEditable(false);
-		TextAreaPrixTotal.setFont(new Font("Ebrima", Font.PLAIN, 14));
-		prixPanel.add(TextAreaPrixTotal);
+		this.lblPrixTotal = new JLabel("-.--€");
+		this.lblPrixTotal.setFont(new Font("Ebrima", Font.BOLD, 15));
+		prixPanel.add(this.lblPrixTotal);
 		
-		JSpinner spinner = new JSpinner();
-		prixPanel.add(spinner);
+		//Création du spinner permettant de choisir la qte d'achat
+		this.spinnerQteAchat = new JSpinner();
+		changementValeurSpinner(this.spinnerQteAchat);
+		this.spinnerQteAchat.setFont(new Font("Ebrima", Font.BOLD, 15));
+		if(tomate.getStock() == 0){
+			this.spinnerQteAchat.setEnabled(false);
+		}
+		else {
+			this.spinnerQteAchat.setModel(new SpinnerNumberModel(1, 1, tomate.getStock(), 1));
+		}
+		prixPanel.add(this.spinnerQteAchat);
+		
+		//Panel contenant les information de prix unitaire et de nombre de graine
+		JPanel specificationPanel = new JPanel();
+		mainPanel.add(specificationPanel);
+		specificationPanel.setOpaque(false);
+		specificationPanel.setLayout(new FlowLayout());
+		
+		//Création du label prix
+		JLabel lblPrix = new JLabel("Prix Unitaire : " + df.format(tomate.getPrixTTC()) + "€");
+		lblPrix.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\coin_resize2.png"));
+		lblPrix.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrix.setFont(new Font("Ebrima", Font.ITALIC, 14));
+		lblPrix.setBorder(new EmptyBorder(0, 10, 10, 10));
+		specificationPanel.add(lblPrix);
+		
+		//Création du label contenant le nombre de graine
+		JLabel lblGraine = new JLabel("Nombre de graine : " + String.valueOf(tomate.getNbGrainesParSachet()));
+		lblGraine.setIcon(new ImageIcon(".\\src\\main\\resources\\images\\Icones\\seed_resize2.png"));
+		lblGraine.setHorizontalAlignment(SwingConstants.CENTER);
+		lblGraine.setFont(new Font("Ebrima", Font.ITALIC, 14));
+		lblGraine.setBorder(new EmptyBorder(0, 10, 10, 10));
+		specificationPanel.add(lblGraine);
+		
 		
 		//Création du panel de bas avec les boutons
-		/*JPanel bottomPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
 		contentPane.add(bottomPanel, BorderLayout.SOUTH);
-		bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		bottomPanel.setLayout(new FlowLayout());
 		
 		//Création du bouton ajout au panier
-		JButton btnNewButton = new JButton("Ajouter au panier");
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setBackground(SystemColor.activeCaption);
-		bottomPanel.add(btnNewButton);
+		JButton btnAjout = new JButton("Ajouter au panier");
+		btnAjout.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnAjout.setForeground(new Color(0, 0, 0));
+		btnAjout.setBackground(SystemColor.activeCaption);
+		btnAjout.setBorder(new EmptyBorder(10, 10, 10, 10));
+		bottomPanel.add(btnAjout);
 		
 		//Création du bouton annuler
-		JButton btnNewButton_1 = new JButton("Annuler");
-		btnNewButton_1.setBackground(SystemColor.activeCaption);
-		bottomPanel.add(btnNewButton_1);*/
+		JButton btnAnnuler = new JButton("Annuler");
+		btnAnnuler.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnAnnuler.setBackground(SystemColor.activeCaption);
+		btnAnnuler.setBorder(new EmptyBorder(10, 10, 10, 10));
+		bottomPanel.add(btnAnnuler);
 		
+		//Appel fonction qui actualise le prix
+		this.actualiserPrix();
 	}
+
+	
+	//Dès que la valeur du spinner change
+	private void changementValeurSpinner(JSpinner spinnerQteAchat) {
+		spinnerQteAchat.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				FenetreProduit.this.actualiserPrix();
+			}
+		});
+	}
+
+	//Actualise le prix totale
+	private void actualiserPrix() {
+		this.lblPrixTotal.setText(df.format((int) this.spinnerQteAchat.getValue() * tomate.getPrixTTC()) + "€");
+	}
+
 }
